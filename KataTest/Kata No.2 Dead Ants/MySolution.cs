9cf36bd3c61da216d72e51938d9d522d9f17455c
th0086 <th00869903@gmail.com>
2020-06-10 10:17:38 +0800

    secondCord

--- KataTest/Kata No.2 Dead Ants/MySolution.cs
+++ KataTest/Kata No.2 Dead Ants/MySolution.cs
@@ -11,8 +11,28 @@ namespace KataTest.Kata_No._2_Dead_Ants
     {
         public static int DeadAntCount(string ants)
         {
-            return -1;
+            int DeadCount = 0;
+
+            if (string.IsNullOrEmpty(ants))
+            {
+                return DeadCount;
+            }
+
+            var antStr = ants.Replace("ant", "").Replace(" ","").Replace(".","");
+            
+            foreach (var ch in "ant")
+            {
+
+                if (DeadCount < antStr.Count(x => x.Equals(ch)))
+                {
+                    DeadCount = antStr.Count(x => x.Equals(ch));
+                }
+
+            }
+            return DeadCount;
+
         }
+
     }
 
 }
