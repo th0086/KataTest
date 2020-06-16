b10ca527c657915b961ce9f98539a8f0f2acfb93
th0086 <th00869903@gmail.com>
2020-06-16 10:17:45 +0800

    Third test

---
+++ KataTest/Kata No.3 Band name generator/MySolution.cs
@@ -0,0 +1,18 @@
+﻿namespace KataTest.Kata_No._3_Band_name_generator
+{
+    public class Kata3
+    {
+        public static string BandNameGenerator(string str)
+        {
+            if (str.Substring(0, 1).Equals(str.Substring(str.Length - 1, 1)))
+            {
+                return str.Substring(0, 1).ToUpper() + str.Substring(1, str.Length - 1) + str.Substring(1, str.Length - 1);
+            }
+            else
+            {
+                return "The" + " " + str.Substring(0, 1).ToUpper() + str.Substring(1, str.Length - 1);
+            }
+
+        }
+    }
+}
---
+++ KataTest/Kata No.3 Band name generator/SampleTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace KataTest.Kata_No._3_Band_name_generator
+{
+    class SampleTest
+    {
+        [TestFixture]
+        public class SolutionTest
+        {
+            [Test]
+            public void SampleTest()
+            {
+                Assert.AreEqual("The Knife", Kata3.BandNameGenerator("knife"));
+                Assert.AreEqual("Tartart", Kata3.BandNameGenerator("tart"));
+                Assert.AreEqual("Sandlesandles", Kata3.BandNameGenerator("sandles"));
+                Assert.AreEqual("The Bed", Kata3.BandNameGenerator("bed"));
+            }
+        }
+    }
+}
